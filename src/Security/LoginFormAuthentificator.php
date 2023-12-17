8e94052b1b9b944c022805afe2f75866aec98e8e
amadou930 <amadoun930@gmail.com>
2023-12-17 16:35:52 +0100

    ajout-style

--- src/Security/LoginFormAuthentificator.php
+++ src/Security/LoginFormAuthentificator.php
@@ -48,7 +48,7 @@ class LoginFormAuthentificator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-        return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
